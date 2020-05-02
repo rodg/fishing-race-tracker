f458f94a485cd7d655ac591d1a0ec2a3bd1c9471
Rod G <rodg1300@gmail.com>
2020-05-02 16:55:19 -0400

    fixed dash doing manual fish entries

--- dashboard/name.js
+++ dashboard/name.js
@@ -3,5 +3,5 @@ var capture_data = nodecg.Replicant('capture_data');
 
 function update() {
 	nodecg.sendMessage('newFishCaught', nameInput.value);
-        capture_data[nameInput.value] = true;
+        capture_data.value[nameInput.value] = true;
 }

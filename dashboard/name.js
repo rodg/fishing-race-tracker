25cadac6e202772b8bb9cd6e668f5fa8f11ab36b
Rod G <rodg1300@gmail.com>
2020-05-02 13:01:04 -0400

    Added panel to manually send fish events

--- dashboard/name.js
+++ dashboard/name.js
@@ -1,14 +1,5 @@
 const nameInput = document.getElementById('name')
-const twitterInput = document.getElementById('twitter')
-const mySong = nodecg.Replicant('mySong');
-
-/*songRep.on('change', newVal => {
-    update();
-});*/
 
 function update() {
-	/*const data = {song: songRep.value.name, 
-                        artist: songRep.value.artist, 
-                        art: songRep.value.albumArt};*/
-	nodecg.sendMessage('showLowerthird');
+	nodecg.sendMessage('newFishCaught', nameInput.value);
 }

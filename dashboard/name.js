5ce10e23a283fd2d94af2a96cabc8294d0595cf4
RodG <rodg1300@gmail.com>
2020-05-05 00:52:31 +0000

    added reset and cleaned up more

--- dashboard/name.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.getElementById('name')
-var capture_data = nodecg.Replicant('capture_data');
-
-function update() {
-	nodecg.sendMessage('newFishCaught', nameInput.value);
-        capture_data.value[nameInput.value] = true;
-}

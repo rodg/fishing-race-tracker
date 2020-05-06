7eab58e4808a2da0f7c8ade126e21246c74e334e
RodG <rodg1300@gmail.com>
2020-05-06 01:16:22 +0000

    push changes to race

--- graphics/race.js
+++ graphics/race.js
@@ -7,9 +7,9 @@ blueData.on('change', (newData) => {
     var caught;
     Object.keys(blueData.value).forEach( (name) => {
         if(blueData.value[name]){
-            caught = document.getElementById(name)
-            caught.classList.add("caught");
+            caught = document.getElementById("blue" + name)
             caught.classList.add("blue");
+            //caught.classList.add("blue");
         }
     });
 });
@@ -18,9 +18,9 @@ redData.on('change', (newData) => {
     var caught;
     Object.keys(redData.value).forEach( (name) => {
         if(redData.value[name]){
-            caught = document.getElementById(name)
-            caught.classList.add("caught");
+            caught = document.getElementById("red" + name)
             caught.classList.add("red");
+            //caught.classList.add("red");
         }
     });
 });
